b59f09efd8942db8c8c4c3017e2ded9929283304
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-12-16 18:22:58 +0100

    Updates

---
+++ precache-manifest.edc6e9ca215d2095747afdb2b3470339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d0c972ea6e4f9d785b67142b3ac8ea",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "c4b3006f3210205e3f5b",
+    "url": "/weatherApp/static/css/main.bf6ed9ce.chunk.css"
+  },
+  {
+    "revision": "ef8253890cc4842c2a33",
+    "url": "/weatherApp/static/js/2.48a19e73.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weatherApp/static/js/2.48a19e73.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4b3006f3210205e3f5b",
+    "url": "/weatherApp/static/js/main.5cc9d7a4.chunk.js"
+  },
+  {
+    "revision": "b1e9a24928adf9e1b54a",
+    "url": "/weatherApp/static/js/runtime-main.1ee812be.js"
+  }
+]);
